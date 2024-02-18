bd235d113c9595718fe06743d1f5177c6dfe3a46
melissa_p <melissa.paschuck@gmail.com>
2024-02-18 10:03:39 -0800

    Added javascript for dark mode button to toggle on/off to light mode. Removed second button from HTML.

--- js/script.js
+++ js/script.js
@@ -5,10 +5,28 @@ title.addEventListener("mouseover", function () {
   title.style.color = "maroon";
 });
 
+
+// ----- Darkmode and light mode toggled on/off with one button ----- //
+
+var darkModeButton = document.querySelector(".dark-mode");
+var theme = document.querySelector("body");
+
+darkModeButton.addEventListener("click", function () {
+  if (theme.classList.contains("dark-palette")) {
+    theme.classList.remove("dark-palette");
+    darkModeButton.innerText = "Dark Mode";
+  } else {
+    theme.classList.add("dark-palette");
+    darkModeButton.innerText = "Light Mode";
+  }
+});
+
+/* ----- Dark mode and light mode applied with two buttons -----
+
 var darkModeButton = document.querySelector(".dark-mode");
 var body = document.querySelector("body");
 
-body.addEventListener("click", function () {
+darkModeButton.addEventListener("click", function () {
   body.classList.add("dark-palette");
 });
 
@@ -17,3 +35,5 @@ var lightModeButton = document.querySelector(".light-mode");
 lightModeButton.addEventListener("click", function () {
   body.classList.remove("dark-palette");
 });
+*/
+
